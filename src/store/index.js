5db3f472c67a6c832473374406ee6feb69fbff96
lzyu0074 <1130443975@qq.com>
2022-04-17 23:37:15 +0800

    初始化了项目并增加了add item功能

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    list: [],
+    inputValue: 'aaa',
+    nextId: 5
   },
   getters: {
   },
   mutations: {
+    setList(state, data) {
+      state.list = data
+    },
+    setInputValue(state, value) {
+      state.inputValue = value
+    },
+    addItem(state) {
+      const obj = {
+        id: state.nextId,
+        info: state.inputValue,
+        done: false
+      }
+      state.list.push(obj)
+      state.inputValue = ''
+      state.nextId++
+    }
   },
   actions: {
+    async getRequestList(context) {
+      const { data: res } = await axios.get('/list.json')
+      // console.log(res)
+      context.commit('setList', res)
+    }
   },
   modules: {
   }
